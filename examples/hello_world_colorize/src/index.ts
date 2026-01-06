890b4567945c057b1e88b1a30a70dc7e89248484
Adam Patterson <adpatter2000@gmail.com>
2026-01-06 18:21:19 -0500

    Improve examples.

--- examples/hello_world_colorize/src/index.ts
+++ examples/hello_world_colorize/src/index.ts
@@ -2,19 +2,21 @@ import { Logger, Formatter, ConsoleHandler, SyslogLevel } from "streams-logger";
 import chalk from "chalk";
 
 const logger = new Logger({ name: "hello-logger", level: SyslogLevel.DEBUG });
+
 const formatter = new Formatter({
   format: ({ isotime, message, name, level, func, line, col }) => {
-    const _level = level != "ERROR" ? chalk.green(level) : chalk.red(level);
-    name = chalk.blue(name);
-    isotime = chalk.grey(isotime);
-    func = chalk.magenta(func);
-    line = chalk.cyan(line);
-    col = chalk.cyan(col);
-    message = chalk.white(message);
+    const _level = level != "ERROR" ? chalk.green(level) : chalk.red(level); // Green or Red
+    name = chalk.blue(name); // Blue
+    isotime = chalk.grey(isotime); // Grey
+    func = chalk.magenta(func); // Magenta
+    line = chalk.cyan(line); // Cyan
+    col = chalk.cyan(col); // Cyan
+    message = chalk.white(message); // White
     const data = `${name}:${isotime}:${_level}:${func}:${line}:${col}:${message}\n`;
     return data;
   },
 });
+
 const consoleHandler = new ConsoleHandler({ level: SyslogLevel.DEBUG });
 
 const log = logger.connect(formatter.connect(consoleHandler));
